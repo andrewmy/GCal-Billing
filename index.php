3aa149d6825c34e14116917cb9ec94b1da6f3755
andr <andrejs@gmail.com>
2012-07-31 02:41:43 -0700

    ok, let the analytics code stay in commits too

--- index.php
+++ index.php
@@ -258,6 +258,15 @@ if (isset($_SESSION['sessionToken']) || isset($_GET['token'])) {
 			var el = document.getElementById(id);
 			el.style.display = (el.style.display == 'none') ? 'block' : 'none';
 		}
+
+		var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-1931660-8']);
+        _gaq.push(['_trackPageview']);
+        (function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		})();
 		</script>
 	</head>
 	<body>
